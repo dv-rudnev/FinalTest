2af7c344976ff6b2b27e8225d1f40955f4d3f6c8
Dmitriy R <yurubu48@gmsil.com>
2022-10-11 22:43:58 +0300

    Доавил код решения задачи. Так же внес актуальные правки в README.md

---
+++ source/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine();
+Console.Write("Введите количество элементов: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+string[] stringArr = new string[len];
+string index = string.Empty;
+string tmp = string.Empty;
+
+for (int i = 0; i < len; i++)
+{
+    Console.Write("Введите {0}-й элемент ({1}): ", i + 1, i);
+    stringArr[i] = Console.ReadLine();
+    if (stringArr[i].Length <= 3)
+    {
+        index += i;
+    }
+}
+Console.WriteLine();
+
+Console.Write("[");
+for (int i = 0; i < stringArr.Length; i++)
+{
+    if (i < stringArr.Length - 1) tmp = ", ";
+    else tmp = "]";
+    Console.Write($"\"{stringArr[i]}\"" + tmp);
+}
+
+Console.Write(" -> ");
+
+Console.Write("[");
+for (int i = 0; i < index.Length; i++)
+{
+    if (i < index.Length - 1) tmp = ", ";
+    else tmp = "]";
+    Console.Write($"\"{stringArr[Convert.ToInt32(Convert.ToString(index[i]))]}\"" + tmp);
+}
+Console.WriteLine();
